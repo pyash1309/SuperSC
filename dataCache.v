a54de0633b50cff6d4723e7ed02b3d5f61bc406c
Yash Purohit <102845610+pyash1309@users.noreply.github.com>
2023-10-29 11:06:07 +0530

    Add files via upload

--- dataCache.v
+++ dataCache.v
@@ -1,5 +1,5 @@
 module dataCache #(
-    parameter cacheSize = 1024,
+    parameter cacheSize = 32,
     parameter cacheWordSize = 8,
     parameter dataSize = 32,
     parameter addrSize = 32
@@ -11,29 +11,50 @@ module dataCache #(
     output [dataSize-1:0] readData1, readData2
 );
 
-reg [cacheWordSize-1:0] cache [0:dataSize*cacheSize/cacheWordSize - 1];
+reg [cacheWordSize-1:0] cacheData [0:((dataSize*cacheSize)/cacheWordSize)-1];
 
 always @(negedge clk) 
 begin
 
     if ((addr1 == addr2) & writeEn1 & writeEn2) 
     begin
-        {cache[addr2], cache[addr2-1], cache[addr2-2], cache[addr2-3]} <= writeData2;    
+        {cacheData[addr2+3], cacheData[addr2+2], cacheData[addr2+1], cacheData[addr2]} <= writeData2;    
     end
 
     else
     begin
         if (writeEn1) begin
-            {cache[addr1], cache[addr1-1], cache[addr1-2], cache[addr1-3]} <= writeData1;
+            {cacheData[addr1+3], cacheData[addr1+2], cacheData[addr1+1], cacheData[addr1]} <= writeData1;
         end 
     
         if (writeEn2) begin
-            {cache[addr2], cache[addr2-1], cache[addr2-2], cache[addr2-3]} <= writeData2;        
+            {cacheData[addr2+3], cacheData[addr2+2], cacheData[addr2+1], cacheData[addr2]} <= writeData2;        
         end
     end
 end
 
-assign readData1 = {cache[addr1], cache[addr1-1], cache[addr1-2], cache[addr1-3]};
-assign readData2 = {cache[addr2], cache[addr2-1], cache[addr2-2], cache[addr2-3]};
+assign readData1 = {cacheData[addr1+3], cacheData[addr1+2], cacheData[addr1+1], cacheData[addr1]};
+assign readData2 = {cacheData[addr2+3], cacheData[addr2+2], cacheData[addr2+1], cacheData[addr2]};
 
 endmodule
+
+/*module dataCache(input clk, write1, write2,
+                   input [31:0] addr1, addr2,
+                   input [31:0] write_data1, write_data2,
+                   output [31:0] read_data1, read_data2);
+
+  reg [31:0] ram [0:63]; //64 words memory
+    
+  always @(negedge clk) begin
+    if((addr1 == addr2) & write1 & write2) ram[addr2[31:2]] <= write_data2;
+    else begin
+      if(write1) ram[addr1[31:2]] <= write_data1;
+      if(write2) ram[addr2[31:2]] <= write_data2;
+    end
+  end
+  
+  assign read_data1 = ram[addr1[31:2]];
+  assign read_data2 = ram[addr2[31:2]];
+
+endmodule
+*/
--- instrCache.v
+++ instrCache.v
@@ -7,15 +7,15 @@ module instrCache #(
     output [instrWidth-1:0] instrF1, instrF2
 );
 
-reg [memWidth-1:0] cache [0:(instrWidth/memWidth)*cacheSize - 1];
+reg [memWidth-1:0] cacheInstr [0:(instrWidth/memWidth)*cacheSize - 1];
 
 initial begin
     integer i;
     for(i=0; i<(instrWidth/memWidth)*cacheSize; i=i+1)
-        cache[i] <= 0;
+        cacheInstr[i] <= 0;
 end
 
-assign instrF1 = {cache[pcF1-3], cache[pcF1-2], cache[pcF1-1], cache[pcF1]};
-assign instrF2 = {cache[pcF2-3], cache[pcF2-2], cache[pcF2-1], cache[pcF2]};
+assign instrF1 = {cacheInstr[pcF1+3], cacheInstr[pcF1+2], cacheInstr[pcF1+1], cacheInstr[pcF1]};
+assign instrF2 = {cacheInstr[pcF2+3], cacheInstr[pcF2+2], cacheInstr[pcF2+1], cacheInstr[pcF2]};
 
 endmodule
